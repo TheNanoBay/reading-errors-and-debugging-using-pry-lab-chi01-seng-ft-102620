30467b9d2af53c28384569216659b335f0281d40
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-09-28 22:07:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,6 +1,5 @@
 # don't forget to add: require 'pry'
 
-require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -13,6 +12,6 @@ end
 
 def engage
   date = generate_star_date
-  binding.pry 
+  puts date 
   puts state_log(date)
 end
